6baa82e7c3c48fea021c0a7fe386b3d1588b8332
kheiakiyama <kheiakiyama@gmail.com>
2017-11-11 14:06:02 +0900

    initial commit created by bot service

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=akiyama-chat-csharp-Web-Deploy.pubxml -p:Password=ZM0Mknq9WSn85jNbKFgGoPYgqrirGix68va2HCzalsZjRAJsSGixanAzuxDp
+
